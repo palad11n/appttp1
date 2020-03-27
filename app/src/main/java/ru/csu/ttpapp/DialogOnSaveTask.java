be2cfa0a881fee891cead2d1dd436939601715f7
Max <uue33@mail.ru>
2020-03-27 21:58:55 +0500

    menu and dialog

---
+++ app/src/main/java/ru/csu/ttpapp/DialogOnSaveTask.java
@@ -0,0 +1,63 @@
+package ru.csu.ttpapp;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.fragment.app.DialogFragment;
+
+public class DialogOnSaveTask extends DialogFragment {
+
+    public interface DialogListener {
+        void onDialogPositiveClick(DialogFragment dialog);
+
+        void onDialogNegativeClick(DialogFragment dialog);
+    }
+
+    public DialogListener mListener;
+    public Button btnYes, btnNo;
+
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            mListener = (DialogListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString() + " must implement DialogListener");
+        }
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        // Get the layout inflater
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        builder.setTitle(R.string.setting_row);
+
+        builder.setView(inflater.inflate(R.layout.dialog_create, null))
+                .setPositiveButton(R.string.done, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        mListener.onDialogPositiveClick(DialogOnSaveTask.this);
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        mListener.onDialogNegativeClick(DialogOnSaveTask.this);
+                    }
+                });
+        return builder.create();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return null;
+    }
+}
